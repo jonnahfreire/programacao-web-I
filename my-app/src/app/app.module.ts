6d052a8dde115224f79a96aeb4202e54270b5023
jonnahfreire <jonnas.freire17@gmail.com>
2022-03-23 17:29:30 -0300

    second commit

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
